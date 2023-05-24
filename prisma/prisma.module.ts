d464776e64353de3688895c9c209a791f9527739
luvlqq <lualgod92@gmail.com>
2023-05-24 16:15:05 +0300

    enable cors and install dependencies

--- prisma/prisma.module.ts
+++ prisma/prisma.module.ts
@@ -4,5 +4,6 @@ import { PrismaService } from './prisma.service';
 @Global() // Global module for all code.
 @Module({
   providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
