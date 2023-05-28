64767fe6a47d18cfb3849b4a43f896a6c04d9cc5
luvlqq <lualgod92@gmail.com>
2023-05-28 15:54:21 +0300

    create a register service and controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, Get, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  login() {}
+  login() {
+    return this.authService.login();
+  }
 
   @Post('register')
-  register() {}
+  register(@Body() dto: RegisterDto) {
+    return this.authService.register(dto);
+  }
 
   @Get('logout')
-  logout() {}
+  logout() {
+    return this.authService.logout();
+  }
 
   @Patch('resetpassword')
-  resetPassword() {}
+  resetPassword() {
+    return this.authService.resetPassword();
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../../prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService,
+  ) {}
+
+  async login() {}
+
+  async register(dto: RegisterDto) {
+    const { email, firstName, lastName, password } = dto;
+    const foundUser = await this.prisma.user.findUnique({ where: { email } });
+    if (foundUser) {
+      throw new BadRequestException(
+        'User with this email is already been exist!',
+      );
+    }
+    const hashedPassword: string = await this.hashPassword(password);
+    await this.prisma.user.create({
+      data: { email, firstName, lastName, hashedPassword },
+    });
+    return { message: 'register was successful ' };
+  }
+
+  async logout() {}
+
+  async resetPassword() {}
+
+  async hashPassword(password: string) {
+    const saltOrRounds = 10;
+    return await bcrypt.hash(password, saltOrRounds);
+  }
+}
