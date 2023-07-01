cbce5c2a1fd8c0b83c26a43769ced82db00a9d3e
luvlqq <lualgod92@gmail.com>
2023-07-01 22:26:06 +0300

    refactor: Delete useless files

--- src/modules/auth/dto/helper.ts
+++
@@ -1,6 +0,0 @@
-export const isNotEmpty = 'The field is cant be empty.';
-export const passwordMatch =
-  'The password must be between 8 and 20 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.';
-export const emailMatch = 'Email is incorrect.';
-export const firstName = 'Incorrect name.';
-export const userLastName = 'Incorrect lastname.';
