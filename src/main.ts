6dc34c3000521a3573ddb58fadfcdd27f1e9c646
luvlqq <lualgod92@gmail.com>
2023-07-06 19:20:33 +0300

    refactor: Add application port to .env

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
